b033aa2788dbb9d017b88b7ea874383735dd3fb4
Corley <grant.corley@sogeti.com>
2022-08-31 14:26:14 -0500

    completed week 1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import PIL
+#!/usr/bin/env python3
+from PIL import Image
+import os
 
+size = (128, 128)
+
+for filename in os.listdir('images'):
+    if filename.startswith('ic_'):
+        icon = Image.open(filename).convert('RGB')
+        icon.rotate(90).resize(size).save('/opt/icons/' + filename, 'JPEG')
