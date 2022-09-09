9b53c1dc703dd917ba80cb92e2a399e4b8d6ac6e
Corley <grant.corley@sogeti.com>
2022-09-09 13:36:39 -0500

    week 3 complete

--- week2.py
+++ week2.py
@@ -3,12 +3,13 @@ import os
 import requests
 
 path = '/data/feedback'
+url = 'http://35.223.61.82/feedback'
+titles = ['title', 'name', 'date', 'feedback']
 for filename in os.listdir(path):
     with open(path + '/' + filename, 'r') as data:
         lines = data.read().splitlines()
         formatted_data = {}
-        titles = ['title', 'name', 'date', 'feedback']
         for i in range(len(titles)):
             formatted_data[titles[i]] = lines[i]
-        response = requests.post('http://34.134.94.14/feedback', data=formatted_data)
+        response = requests.post(url, data=formatted_data)
         print(str(response.status_code))
